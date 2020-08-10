6dcf310a9ed4bf944cd08047d587b9119769620d
AmSmo <adam.smolenski@gmail.com>
2020-08-10 17:42:01 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,41 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    self.all.sum { |artist| artist.years_experience }
+  end
+
+  def self.most_prolific
+    self.all.max_by {|artist| (artist.paintings.length*1.0/artist.years_experience) }
+  end
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def paintings
+    Painting.all.select {|painting| painting.artist == self }
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery }
+  end
+
+  def cities 
+    galleries.map {|gallery| gallery.city}
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,19 @@
 require_relative '../config/environment.rb'
 
+bob = Artist.new("Bob Ross", 40)
+monet = Artist.new("Monet", 20)
+picasso = Artist.new("Picasso", 10)
 
+louvre = Gallery.new("Louvre", "Paris")
+moma = Gallery.new("Moma", "New York")
+gug = Gallery.new("Guggenheim", "New York")
+met = Gallery.new("Met", "Chicago")
 
+bob.create_painting("Happy Trees", 10000, moma)
+weeping = Painting.new("Weeping woman", 13, louvre, picasso)
+weeping2 = Painting.new("Weeping woman", 12, louvre, picasso)
+weeping3 = Painting.new("Weeping woman", 12, louvre, picasso)
+sunrise = Painting.new("Sunrise", 30000, gug, monet)
 
 binding.pry
 
